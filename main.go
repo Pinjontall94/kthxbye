f2455f84d2f308d4cf4c71367b56d15e30b47f33
Sam Johnson <sambjohnson94@gmail.com>
2024-04-21 20:46:43 -0700

    add excelize skeleton

--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import(
 	"fmt"
+	"log"
+	"regexp"
+
 	"github.com/gocolly/colly"
+	"github.com/xuri/excelize/v2"
 )
 
 func main() {
+	base_url := "https://edd.ca.gov"
+	warn_report_url := base_url + "/en/jobs_and_training/Layoff_Services_WARN"
+	warn_xlsx_url := base_url + "/siteassets/files/jobs_and_training/warn"
+	pattern := regexp.QuoteMeta(warn_xlsx_url) + "/warn_report1\\.xlsx"
+	strings_lst := [...]string{base_url, warn_report_url, warn_xlsx_url, pattern}
+	for _, str := range strings_lst {
+		fmt.Printf("%s\n", str)
+	}
+
 	c := colly.NewCollector(
 		colly.AllowedDomains("edd.ca.gov"),
+		colly.URLFilters(regexp.MustCompile(pattern)),
 	)
-	ca_warn_url := "https://edd.ca.gov/en/jobs_and_training/Layoff_Services_WARN"
 
 	// place a callback on every a tag with an href
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
@@ -25,6 +38,19 @@ func main() {
 		fmt.Println("Visiting...", r.URL.String())
 	})
 
-	c.Visit(ca_warn_url)
+	c.OnResponse(func(r *colly.Response) {
+		warn_report := excelize.NewFile()
+		defer func() {
+			if err := warn_report.Close(); err != nil {
+				fmt.Println(err)
+			}
+		}()
+		//err := warn_report.Read(r.Body)
+	})
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong:", err)
+	})
+
+	c.Visit(warn_report_url)
 	fmt.Println("henlo! :3")
 }
